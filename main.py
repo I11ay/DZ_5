f007b01eba74790bab1a466ec7bfbcd1397f3c09
I11ay <pro100otec131@gmail.com>
2022-09-18 23:29:29 +0300

    DZ_5
    Обновленное

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 number = int(input('Введите любое трёхзначное число:'))
-first_digit = int(int(number) / 100)
-print(first_digit)
-
-"""
-second_digit = int(number - first_digit * 100 / 10)
-third_digit = int(number - first_digit * 100 - second_digit * 10)
+first_digit = int(number / 100)
+second_digit = int((number - first_digit * 100) / 10)
+third_digit = int(number % 10)
 reversed_number = int(str(third_digit) + str(second_digit) + str(first_digit))
 print(reversed_number)
-"""
-фыапфаы
-
-фап
